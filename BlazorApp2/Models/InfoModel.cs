2ffa97f04e7566d0e3fb12e26b30c2738dce30d7
franc <franc@FRANKENSTEIN>
2022-05-16 13:13:24 +0200

    Add project files.

---
+++ BlazorApp2/Models/InfoModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorApp2.Models
+{
+    public class InfoModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ID { get; set; }
+
+        public string Email { get; set; }
+    }
+}
